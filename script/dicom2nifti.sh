02920b9b0c5ef7d2342b0cc027a2c158aabbda4b
logannii <loganzhang0416@gmail.com>
2023-02-08 11:03:14 +0000

    Adds preprocessing script dicom2nifti.sh

---
+++ script/dicom2nifti.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+datadir="/Users/xinzhang/Downloads/mrc_asl_cic/data"
+
+for sub in "sub01" "sub02" "sub03" "sub04" "sub05" "sub06" "sub07"
+do
+    for scanner in "ge" "ing"
+    do 
+        for session in "s1" "s2"
+        do 
+            cd $datadir"/"$sub"/"$scanner"/"$session
+            mkdir nifti
+            dcm2niix -o nifti -f %d -g i -z y dicom
+        done
+    done
+done
